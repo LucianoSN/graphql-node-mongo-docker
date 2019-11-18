7ae2f9e6bb54fc3aa7a0c8b24af1374649be0a17
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-18 16:42:07 -0300

    configurando eslint + prettier

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,40 +1,40 @@
-import {resolve} from 'path';
-import {GraphQLServer} from 'graphql-yoga'
+import { resolve } from 'path';
+import { GraphQLServer } from 'graphql-yoga';
 
 const typeDefs = resolve(__dirname, 'schema.graphql');
 
 const USERS = [
-    {id: 1, name: 'Tony Stark', email: 'ironman@marvel.com'},
-    {id: 2, name: 'Peter Parker', email: 'spiderman@marvel.com'},
-    {id: 3, name: 'Steve Rogers', email: 'captain@marvel.com'}
+	{ id: 1, name: 'Tony Stark', email: 'ironman@marvel.com' },
+	{ id: 2, name: 'Peter Parker', email: 'spiderman@marvel.com' },
+	{ id: 3, name: 'Steve Rogers', email: 'captain@marvel.com' },
 ];
 
 const resolvers = {
-    User: {
-        name: (parent) => {
-            console.log('PARENT', parent);
-            return `User: ${parent.name}`;
-        }
-    },
-    Query: {
-        users: () => USERS
-    },
-    Mutation: {
-        createUser: (parent, args, ctx, info) => {
-            const { data } = args;
-            const user = {
-                ...data,
-                id: USERS.length + 1
-            };
-            USERS.push(user);
-            return user;
-        }
-    }
+	User: {
+		name: (parent): string => {
+			// console.log('PARENT', parent);
+			return `User: ${parent.name}`;
+		},
+	},
+	Query: {
+		users: (): typeof USERS => USERS,
+	},
+	Mutation: {
+		createUser: (parent, args): typeof USERS[0] => {
+			const { data } = args;
+			const user = {
+				...data,
+				id: USERS.length + 1,
+			};
+			USERS.push(user);
+			return user;
+		},
+	},
 };
 
 const server = new GraphQLServer({
-    resolvers,
-    typeDefs,
+	resolvers,
+	typeDefs,
 });
 
 export default server;
