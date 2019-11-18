2ae4017e4f27f67f32fdac9e5e35f66233157724
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-18 14:11:00 -0300

    implementando primeira query

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,8 +3,20 @@ import {GraphQLServer} from 'graphql-yoga'
 
 const typeDefs = resolve(__dirname, 'schema.graphql');
 
+const USERS = [
+    {id: 1, name: 'Tony Stark'},
+    {id: 2, name: 'Spider Man'},
+];
+
+const resolvers = {
+    Query: {
+        users: () => USERS
+    }
+};
+
 const server = new GraphQLServer({
-    typeDefs
+    resolvers,
+    typeDefs,
 });
 
 export default server;
