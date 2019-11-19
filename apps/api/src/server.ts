ddca647c9c71e45cdc3545d491248641b38dc39d
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-19 19:49:26 -0300

    models do mongoose dentro do contex dos resolvers

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,5 +1,7 @@
 import { resolve } from 'path';
 import { GraphQLServer } from 'graphql-yoga';
+import { models as db } from './models';
+import { Resolver } from './types';
 
 const typeDefs = resolve(__dirname, 'schema.graphql');
 
@@ -8,6 +10,20 @@ const USERS = [
 	{ id: 2, name: 'Peter Parker', email: 'spiderman@marvel.com' },
 	{ id: 3, name: 'Steve Rogers', email: 'captain@marvel.com' },
 ];
+
+const createUser: Resolver<{ data: any }> = (
+	parent,
+	args,
+	ctx
+): typeof USERS[0] => {
+	const { data } = args;
+	const user = {
+		...data,
+		id: USERS.length + 1,
+	};
+	USERS.push(user);
+	return user;
+};
 
 const resolvers = {
 	User: {
@@ -19,22 +35,13 @@ const resolvers = {
 	Query: {
 		users: (): typeof USERS => USERS,
 	},
-	Mutation: {
-		createUser: (parent, args): typeof USERS[0] => {
-			const { data } = args;
-			const user = {
-				...data,
-				id: USERS.length + 1,
-			};
-			USERS.push(user);
-			return user;
-		},
-	},
+	Mutation: {},
 };
 
 const server = new GraphQLServer({
 	resolvers,
 	typeDefs,
+	context: { db },
 });
 
 export default server;
