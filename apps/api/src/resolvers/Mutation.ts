d94db8d926d57cf90afbdf0bc11b3a7ce02e68d2
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-21 15:24:03 -0300

    mutation para deletar produto por id

--- apps/api/src/resolvers/Mutation.ts
+++ apps/api/src/resolvers/Mutation.ts
@@ -1,4 +1,4 @@
-import { ProductCreateInput, Resolver } from '../types';
+import { ProductCreateInput, ProductDeleteInput, Resolver } from '../types';
 
 const createProduct: Resolver<ProductCreateInput> = (_, args, { db }) => {
 	const { Product } = db;
@@ -8,6 +8,14 @@ const createProduct: Resolver<ProductCreateInput> = (_, args, { db }) => {
 	return product.save();
 };
 
+const deleteProduct: Resolver<ProductDeleteInput> = async (_, args, { db }) => {
+	const { Product } = db;
+	const { _id } = args;
+
+	return Product.findByIdAndRemove(_id);
+};
+
 export default {
 	createProduct,
+	deleteProduct,
 };
