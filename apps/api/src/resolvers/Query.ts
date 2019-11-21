6a2160eb879f34b4f5ef76592b4e211fd59f9f00
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-21 12:20:20 -0300

    query para listar produtos

---
+++ apps/api/src/resolvers/Query.ts
@@ -0,0 +1,5 @@
+import { Resolver } from '../types';
+
+const products: Resolver<{}> = (_, args, ctx) => ctx.db.Product.find();
+
+export default { products };
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,42 +1,9 @@
 import { resolve } from 'path';
 import { GraphQLServer } from 'graphql-yoga';
 import { models as db } from './models';
-import { Resolver } from './types';
+import resolvers from './resolvers';
 
 const typeDefs = resolve(__dirname, 'schema.graphql');
-
-const USERS = [
-	{ id: 1, name: 'Tony Stark', email: 'ironman@marvel.com' },
-	{ id: 2, name: 'Peter Parker', email: 'spiderman@marvel.com' },
-	{ id: 3, name: 'Steve Rogers', email: 'captain@marvel.com' },
-];
-
-const createUser: Resolver<{ data: any }> = (
-	parent,
-	args,
-	ctx
-): typeof USERS[0] => {
-	const { data } = args;
-	const user = {
-		...data,
-		id: USERS.length + 1,
-	};
-	USERS.push(user);
-	return user;
-};
-
-const resolvers = {
-	User: {
-		name: (parent): string => {
-			// console.log('PARENT', parent);
-			return `User: ${parent.name}`;
-		},
-	},
-	Query: {
-		users: (): typeof USERS => USERS,
-	},
-	Mutation: {},
-};
 
 const server = new GraphQLServer({
 	resolvers,
