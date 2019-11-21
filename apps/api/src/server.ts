8c24c4b34c9dd98037353987959f142ccd4ca23e
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-21 17:23:29 -0300

    usando middlewares

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { GraphQLServer } from 'graphql-yoga';
 import { models as db } from './models';
 import resolvers from './resolvers';
+import { logMiddleware } from './middlewares';
 
 const typeDefs = resolve(__dirname, 'schema.graphql');
 
@@ -9,6 +10,7 @@ const server = new GraphQLServer({
 	resolvers,
 	typeDefs,
 	context: { db },
+	middlewares: [logMiddleware],
 });
 
 export default server;
