2b0927dc20c409aaa0ae9551d7f98292d84fa140
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-21 19:23:30 -0300

    implementando classe customError

---
+++ apps/api/src/erros/CustomError.ts
@@ -0,0 +1,28 @@
+class CustomError extends Error {
+	extensions: Record<string, any>;
+
+	constructor(
+		message: string,
+		code: string,
+		extensions?: Record<string, any>
+	) {
+		super(message);
+
+		const name: string = code
+			.split('_')
+			.map(
+				word =>
+					word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+			)
+			.join('');
+
+		Object.defineProperty(this, 'name', { value: name });
+
+		this.extensions = {
+			code,
+			...extensions,
+		};
+	}
+}
+
+export { CustomError };
