8d85627ae323314c0b11ab394d83611532416227
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-24 17:28:24 -0300

    ajustando nome das interfaces

--- apps/api/src/utils.ts
+++ apps/api/src/utils.ts
@@ -1,6 +1,6 @@
 import { Document, Model, Types } from 'mongoose';
 import {
-	CheckExistenceOptions,
+	FindDocumentOptions,
 	OrderItemSubdocument,
 	TokenPayload,
 } from './types';
@@ -10,7 +10,7 @@ import { sign, SignOptions } from 'jsonwebtoken';
 const isMongoId = (value: string): boolean => Types.ObjectId.isValid(value);
 
 const findDocument = async <T extends Document>(
-	opts: CheckExistenceOptions
+	opts: FindDocumentOptions
 ): Promise<T> => {
 	const {
 		model,
